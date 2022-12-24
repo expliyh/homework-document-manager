047c0eff6237b10881aa5be269065edfe221f621
Expliyh <expliyh@outlook.com>
2022-12-24 10:47:00 +0800

    Web02

--- src/main/java/top/expli/webapi/SocketServer.java
+++ src/main/java/top/expli/webapi/SocketServer.java
@@ -1,6 +1,7 @@
 package top.expli.webapi;
 
 import top.expli.Config;
+import top.expli.Token;
 
 import javax.swing.*;
 import java.io.BufferedReader;
@@ -13,6 +14,8 @@ import java.net.SocketImpl;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SocketServer extends ServerSocket {
     private ExecutorService pool;
@@ -20,7 +23,6 @@ public class SocketServer extends ServerSocket {
     public SocketServer() throws IOException {
         super(Config.SERVER_PORT);
         pool = Executors.newCachedThreadPool();
-
         try {
             while (true) {
                 Socket socket = accept();
@@ -40,10 +42,40 @@ public class SocketServer extends ServerSocket {
         private PrintWriter printWriter;
         private String userName;
         private String permissionLevel;
+        private ScheduledExecutorService heartBeatChecker;
+        private long lastHeartBeat;
+        private long lastOperate;
 
         @Override
         public void run() {
+            lastHeartBeat = System.currentTimeMillis();
+            heartBeatChecker = Executors.newSingleThreadScheduledExecutor();
+            HeartBeatChecker checker = new HeartBeatChecker(this);
+            heartBeatChecker.scheduleWithFixedDelay(checker,0,20, TimeUnit.SECONDS);
+            lastOperate = System.currentTimeMillis();
+            while (true) {
+                System.out.println("New run " + Token.randomString(10));
+                try {
+                    printWriter.println("Received: " + reader.readLine());
+                } catch (IOException e) {
+                    return;
+                }
+            }
+        }
 
+        class HeartBeatChecker extends Thread{
+            RequestHandler handler;
+            public HeartBeatChecker(RequestHandler handler){
+                this.handler = handler;
+            }
+            @Override
+            public void run() {
+                if(System.currentTimeMillis()-lastHeartBeat>10*1000){
+                    handler.interrupt();
+                }
+                System.out.println("Heart beat checked!");
+                System.out.println(handler.isInterrupted());
+            }
         }
 
         public RequestHandler(Socket client) throws IOException {
@@ -51,7 +83,14 @@ public class SocketServer extends ServerSocket {
             this.reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
             this.printWriter = new PrintWriter(client.getOutputStream(), true);
             System.out.println("New client connected!");
-            start();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (SocketServer server = new SocketServer()) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
